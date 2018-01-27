66f08e8817cbe0093a808e77fb838faa91548a30
Jesandry <jesandry@gmail.com>
2018-01-28 04:07:11 +0800

    added sitting timer

--- Camera/streamfirebase.py
+++ Camera/streamfirebase.py
@@ -3,17 +3,16 @@ import _thread
 import time
 
 db = None
-
-def initCamera(userID, cameraID):
-	user = userID + "," + cameraID
+cameraID = '12'
 
 def stream_handler(message):
 	global db
+	global cameraID
 	try:
 		if message['event'] == 'put':
 			path = message['path'].split(',')[1].split('/')[0]
 			#hard coded camera ID
-			if path == '12' and message['data'] == 'True':
+			if path == cameraID and message['data'] == 'True':
 				take_picture(False)
 				data = {"calibrate": "False"}
 				db.child("users").child(message['path'].split(',')[0].split('/')[1] + ',' + path).set(data)
@@ -23,12 +22,26 @@ def stream_handler(message):
 def run_stream(db):
 	my_stream = db.child("users").stream(stream_handler)
 
-def check_posture():
+def check_posture(db, cameraID):
+	time_count = 0
 	while True:
-		take_picture(True)
+		sit_check = take_picture(True)
 		time.sleep(5)
+		if sit_check == True:
+			time_count += 5
+			if time_count > 1200:
+				users_list = db.child("users").get()
+				for user in users_list.each():
+					#hard coded camera ID
+					if user.key().split(',')[1] == cameraID:
+						data = {"timer": time_count, "seat_exceeded": "True"}
+						db.child("users").child(user.key()).set(data)
+		else:
+			time_count = 0
 		
 def take_picture(check):
+	global db
+	global cameraID
 	#TODO: Add RPI camera code
 	if check:
 		print ("test")
@@ -37,22 +50,23 @@ def take_picture(check):
 		users_list = db.child("users").get()
 		for user in users_list.each():
 			#hard coded camera ID
-			if user.key().split(',')[1] == 12:
+			if user.key().split(',')[1] == cameraID:
 				storage.child("users").child(user.key() + "/Image.jpg").put("asd.jpeg", uid)
 				url = storage.child("users").child(user.key() + "/Image.jpg").get_url(uid)
 				data = {"UserID" : user.key(), "Image" : url}
 				db.child("users").child(user.key() + "/Images.jpg").set(data)
 	
 def main():
+	global db
+	global cameraID
 	config = {}
 	f = open("config.txt", "r")
 	for line in f:
 		pair = line.split(':', 1)
 		config[pair[0]] = pair[1].split('\n')[0]
 	f.close()
-
+	
 	firebase = pyrebase.initialize_app(config)
-	global db
 	db = firebase.database()
 	storage = firebase.storage()
 	additional_claims = {
@@ -65,7 +79,7 @@ def main():
 
 	try:
 	   _thread.start_new_thread(run_stream, (db,))
-	   #_thread.start_new_thread(check_posture, ())
+	   #_thread.start_new_thread(check_posture, (db,cameraID, ))
 	except:
 	   print ("Error: unable to start thread")
 	   

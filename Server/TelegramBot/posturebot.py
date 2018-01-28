897d77b834f376470e29ca9e70f5881db8221893
Jackie Tan <free-soup@hotmail.com>
2018-01-28 11:40:34 +0800

    Allow on-ing the alert and receive message for certain variables

--- Server/TelegramBot/posturebot.py
+++ Server/TelegramBot/posturebot.py
@@ -2,20 +2,20 @@ import time, datetime
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
-from server import requestTakePic
-from server import requestNewUser
-
+from server import requestTakePic, requestNewUser, start_stream, close_stream
 now = datetime.datetime.now()
 
 cameraId = 0
+user_id = 0
 
 def loopAction(msg):
     global cameraId
+    global user_id
     content_type, chat_type, user_id = telepot.glance(msg)
 
     keyboard = ReplyKeyboardMarkup(keyboard = 
-        [KeyboardButton(text = "/setup"), KeyboardButton(text = "/calibrate")
-        ], one_time_keyboard = True, resize_keyboard = True
+        [[KeyboardButton(text = "/setup"), KeyboardButton(text = "/calibrate")
+        ]], one_time_keyboard = True, resize_keyboard = True
     )
     
     if content_type == 'text':
@@ -41,15 +41,20 @@ def loopAction(msg):
                 telegram_bot.sendMessage(user_id, "Camera ID verification error")
         #to reset reference image
         elif commandls[0] == "/calibrate":
-            if cameraId!=0:
+            if cameraId!=None:
                 requestTakePic(str(user_id) + "," + cameraId)
                 #run Jackie's script to ping firebase
             else:
-                telegram_bot.sendMessage(user_id, "no camera")
+                telegram_bot.sendMessage(user_id, "Please setup your camera via /setup.")
         #keyboard setting
-        elif commandls[0] == "/bot":
+        elif commandls[0] == "/help":
             telegram_bot.sendMessage(user_id, "Please select an option", reply_markup = keyboard, )
-                                     
+        elif commandls[0] == "/on":
+            print(cameraId)
+            start_stream(user_id, cameraId, telegram_bot)
+        elif commandls[0] == "/off":
+            close_stream()
+                                
 
 telegram_bot = telepot.Bot('382774272:AAHvT_9m9IW3u5q0NJM5U389RMqzStID22o')
 
@@ -58,3 +63,4 @@ print('Listening...')
 
 while 1:
     time.sleep(10)
+
--- Server/TelegramBot/server.py
+++ Server/TelegramBot/server.py
@@ -1,6 +1,15 @@
 import pyrebase
+import _thread
+import telepot
+
+db = None
+my_stream =None
+userID = None
+cameraID = None
+telebot = None
 
 def requestTakePic(childName):
+    global db
     config = getConfig()
 
     firebase = pyrebase.initialize_app(config)
@@ -20,6 +29,7 @@ def requestTakePic(childName):
     db.child("users").child(childName).update({"calibrate":True}, uid)
 
 def requestNewUser(childName, data):
+    global db
     config = getConfig()
 
     firebase = pyrebase.initialize_app(config)
@@ -47,5 +57,62 @@ def getConfig():
         config[pair[0]] = pair[1].split('\n')[0]
     f.close()
     return config
+
+def stream_handler(message):
+    global db
+    global cameraID
+    global userID
+    try:
+        if message['event'] == 'put':
+            print(message['data'])
+            path = message['path'].split(',')[1].split('/')[0]
+            path2 = message['path'].split(',')[0].split('/')[1]
+            variable = message['path'].split('/')[2]
+            cmessage = message['data']
+            print(cameraID == path)
+            print(userID == path2)
+            print(variable == "posture")
+            print(cmessage == True)
+
+            if path == cameraID and path2 == str(userID):
+                if variable == "seat_exceeded" and cmessage == True:
+                    telebot.sendMessage(path2, "You sit too long")
+                    data = {"seat_exceeded": False}
+                    db.child("users").child(path2+','+path).update(data)
+                elif variable == "posture" and cmessage == True:
+                    print("Hello")
+                    telebot.sendMessage(path2, "Your posture bro")
+                    data = {"posture": False}
+                    db.child("users").child(path2+','+path).update(data)
+
+    except Exception as e:
+        print(e)
+
+def start_stream(user, camera, telegram_bot):
+    global telebot
+    global cameraID
+    global userID
+    global my_stream
+
+    telebot = telegram_bot
+
+    userID = user
+    cameraID = camera
+
+    _thread.start_new_thread(run_stream, (db,))
+
+def run_stream(db):
+    global my_stream
+    my_stream = db.child("users").stream(stream_handler)
+
+def close_stream():
+    global my_stream
+    my_stream.close()
+
+
+
+
+
+
 
     
